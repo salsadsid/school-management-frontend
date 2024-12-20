8c480aba1d214e0460a689f69d2a4f02156ea8f2
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2024-12-21 00:41:20 +0600

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
-  build: {
-    commonjsOptions: { transformMixedEsModules: true }, // Change
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 });
