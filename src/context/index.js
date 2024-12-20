569795d1af6c0761c8d0c9ede6979a49650c2437
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2024-12-21 00:31:23 +0600

    jsx to js

--- src/context/index.js
+++ src/context/index.js
@@ -1,5 +1,5 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
 
 export const MaterialTailwind = React.createContext(null);
 MaterialTailwind.displayName = "MaterialTailwindContext";
