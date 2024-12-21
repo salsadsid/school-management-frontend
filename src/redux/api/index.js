0607c3f1b4aaa23722cf5b1f979dcc9d7b434002
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2024-12-21 16:15:45 +0600

    production api

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/api/v1",
+  baseUrl: "https://school-management-backend-uhc1.onrender.com/api/v1",
   prepareHeaders: (headers, { getState }) => {
     const token = getState()?.auth?.token;
     // console.log(token);
